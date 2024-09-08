f21de32e1c2214c4e389a980f8dc6c61940c3ede
SirKaelan <kaelanfreakerman6@gmail.com>
2024-09-08 21:14:03 +0300

    Additional file and folder set up

--- mozilla-website/src/App.tsx
+++ mozilla-website/src/App.tsx
@@ -1,5 +1,18 @@
+import { Navigation } from "src/components/Navigation";
+import { Footer } from "src/components/Footer";
+import { Header } from "src/components/Header";
+
 function App() {
-  return <p className="text-2xl font-bold underline">Hello World!</p>;
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <main>
+        <p className="text-2xl font-bold underline">Main website content</p>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
