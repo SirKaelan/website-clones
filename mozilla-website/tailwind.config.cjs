b1610ae46edbc34db11e681a95a3521e395bd4f5
SirKaelan <kaelanfreakerman6@gmail.com>
2024-09-09 11:29:34 +0300

    Collect additional assets and add new fonts to Tailwind

--- mozilla-website/tailwind.config.cjs
+++ mozilla-website/tailwind.config.cjs
@@ -1,8 +1,15 @@
+const default_theme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...default_theme.fontFamily.sans],
+        heading: ["Zilla Slab", ...default_theme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
