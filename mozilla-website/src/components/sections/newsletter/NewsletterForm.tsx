aa3a7c341e8e047211f1aab7b1fd3d0cfce7cf35
sirkaelan <kaelanfreakerman6@gmail.com>
2024-10-18 20:26:11 +0300

    Add initial form validation on submit

--- mozilla-website/src/components/sections/newsletter/NewsletterForm.tsx
+++ mozilla-website/src/components/sections/newsletter/NewsletterForm.tsx
@@ -1,18 +1,80 @@
 import { useState } from "react";
 import { Button } from "src/components/Button";
 
+type FormState = {
+  email: string;
+  country: string;
+  lang: string;
+  privacy: boolean;
+};
+
+type FormErrors = {
+  email: string;
+  privacy: string;
+};
+
 export const NewsletterForm = () => {
   const [emailInteracted, setEmailInteracted] = useState<boolean>(false);
-  // TODO: state for form values
+  // TODO: Do something about hardcoded country and lang codes
+  const [formValues, setFormValues] = useState<FormState>({
+    email: "",
+    country: "bg",
+    lang: "bg",
+    privacy: false,
+  });
+
+  const [formErrors, setFormErrors] = useState<FormErrors>({
+    email: "",
+    privacy: "",
+  });
 
   const handleEmailClick = () => {
     if (emailInteracted) return;
     setEmailInteracted(true);
   };
 
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      email: e.target.value,
+    }));
+  };
+
+  const handleCountryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      country: e.target.value,
+    }));
+  };
+
+  const handleLangChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      lang: e.target.value,
+    }));
+  };
+
+  const handlePrivacyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues((prevState) => ({
+      ...prevState,
+      privacy: e.target.checked,
+    }));
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // TODO: clear form state values
+
+    const errors: FormErrors = {
+      email: validateEmail(formValues.email),
+      privacy: validatePrivacy(formValues.privacy),
+    };
+
+    setFormErrors(errors);
+
+    if (Object.values(errors).filter((error) => error !== "").length === 0) {
+      console.log("Form submitted:", formValues);
+      return;
+    }
   };
 
   return (
@@ -20,6 +82,9 @@ export const NewsletterForm = () => {
       <form className="mb-6" onSubmit={handleSubmit}>
         <fieldset className="mb-6 flex flex-col gap-5">
           <div>
+            {formErrors.email && (
+              <div className="text-red-500">{formErrors.email}</div>
+            )}
             <label htmlFor="id_email" className="block text-sm font-bold pb-2">
               Your email address:
             </label>
@@ -30,6 +95,8 @@ export const NewsletterForm = () => {
               placeholder="yourname@example.com"
               className="p-2 w-full border-2 rounded border-gray-400 hover:border-blue-800 placeholder-gray-500"
               onClick={handleEmailClick}
+              onChange={handleEmailChange}
+              value={formValues.email}
             />
           </div>
 
@@ -46,8 +113,9 @@ export const NewsletterForm = () => {
                 <select
                   id="id_country"
                   name="country"
-                  defaultValue="bg"
                   className="p-2 w-full bg-white border-2 rounded border-gray-400 hover:border-blue-800"
+                  onChange={handleCountryChange}
+                  value={formValues.country}
                 >
                   <option value="bg">Bulgaria</option>
                   <option value="uk">United Kingdom</option>
@@ -66,8 +134,9 @@ export const NewsletterForm = () => {
                 <select
                   id="id_lang"
                   name="lang"
-                  defaultValue="bg"
                   className="p-2 w-full bg-white border-2 rounded border-gray-400 hover:border-blue-800"
+                  onChange={handleLangChange}
+                  value={formValues.lang}
                 >
                   <option value="bg">Bulgarian</option>
                   <option value="uk">English</option>
@@ -77,8 +146,17 @@ export const NewsletterForm = () => {
               </div>
 
               <div>
+                {formErrors.privacy && (
+                  <div className="text-red-500">{formErrors.privacy}</div>
+                )}
                 <label htmlFor="privacy" className="text-sm font-bold">
-                  <input id="privacy" type="checkbox" className="mr-2" />
+                  <input
+                    id="privacy"
+                    type="checkbox"
+                    className="mr-2"
+                    onChange={handlePrivacyChange}
+                    checked={formValues.privacy}
+                  />
                   <span className="mr-1">
                     I’m okay with Mozilla handling my info as explained in
                   </span>
@@ -104,4 +182,17 @@ export const NewsletterForm = () => {
       </form>
     </div>
   );
+};
+
+// Form field validators
+const validateEmail = (email: string): string => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) return "Email is required";
+  if (!emailRegex.test(email)) return "Invalid email";
+  return "";
+};
+
+const validatePrivacy = (privacy: boolean): string => {
+  if (!privacy) return "You must agree to the privacy policy";
+  return "";
 };
