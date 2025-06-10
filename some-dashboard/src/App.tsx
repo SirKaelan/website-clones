381191a5790d6d350687959ae23d5361b1ab1f5f
SirKaelan <kaelanfreakerman6@gmail.com>
2025-06-10 16:47:35 +0300

    refactor(stats-card): apply the compound component pattern

--- some-dashboard/src/App.tsx
+++ some-dashboard/src/App.tsx
@@ -4,9 +4,22 @@ import { StatsCard } from "./components/StatsCard";
 function App() {
   return (
     <>
-      <div className="p-6">
+      <div className="flex flex-col gap-9 p-6">
         <FloatingHeader />
-        <StatsCard />
+        <StatsCard>
+          <StatsCard.Header>
+            <StatsCard.Header.Icon className="red-gradient-bg red-shadow">
+              <svg></svg>
+            </StatsCard.Header.Icon>
+
+            <StatsCard.Header.Stats label="Bookings" value="281" />
+          </StatsCard.Header>
+
+          <StatsCard.Content>
+            <StatsCard.Content.Trend value="+55%" timeframe="than last week" />
+          </StatsCard.Content>
+        </StatsCard>
+
         <span className="text-4xl">Dashyboard</span>
       </div>
     </>
--- some-dashboard/src/components/StatsCard.tsx
+++ some-dashboard/src/components/StatsCard.tsx
@@ -1,25 +1,100 @@
+import React from "react";
+import type { JSX } from "react";
 import { Card, CardHeader, CardContent } from "./ui/card";
+import { cn } from "@/lib/utils";
 
-export const StatsCard = () => {
+export const StatsCard: StatsCardType = ({ children }) => {
+  const subComponents = React.Children.toArray(children);
+  const headerChild = subComponents.find(
+    (c) => React.isValidElement(c) && c.type === StatsCard.Header
+  );
+  const contentChild = subComponents.find(
+    (c) => React.isValidElement(c) && c.type === StatsCard.Content
+  );
+
   return (
-    <Card className="flex flex-col gap-0 shadow-md p-4">
-      <CardHeader className="flex justify-between p-0">
-        {/* icon container */}
-        <div className="h-16 w-16 -mt-8 flex justify-center items-center bg-pink-200 rounded-xl">
-          Icon
-        </div>
-        {/* stat data container */}
-        <div className="flex flex-col items-end">
-          <span className="text-gray-500 font-thin">Bookings</span>
-          <span className="text-2xl font-bold text-blue-950">281</span>
-        </div>
-      </CardHeader>
-      <hr className="hr-fade"></hr>
-      <CardContent className="p-0">
-        <p className="text-gray-500 font-thin">
-          <span className="text-green-500 font-bold">+55%</span> than last week
-        </p>
-      </CardContent>
+    <Card className="flex flex-col gap-8 shadow-md p-4">
+      {headerChild && headerChild}
+      {contentChild && contentChild}
     </Card>
   );
+};
+
+// Header subcomponent
+const StatsCardHeader: StatsCardType["Header"] = ({ children }) => {
+  const subComponents = React.Children.toArray(children);
+  const iconChild = subComponents.find(
+    (c) => React.isValidElement(c) && c.type === StatsCard.Header.Icon
+  );
+  const statsChild = subComponents.find(
+    (c) => React.isValidElement(c) && c.type === StatsCard.Header.Stats
+  );
+
+  return (
+    <CardHeader className="flex justify-between p-0">
+      {iconChild && iconChild}
+      {statsChild && statsChild}
+    </CardHeader>
+  );
+};
+// TODO: Change this to accept the SVG
+StatsCardHeader.Icon = ({ className, children }) => {
+  const baseClasses =
+    "h-16 w-16 -mt-8 flex justify-center items-center bg-pink-200 rounded-xl";
+  return <div className={cn(baseClasses, className)}>Icon</div>;
+};
+StatsCardHeader.Stats = ({ label, value }) => (
+  <div className="flex flex-col items-end">
+    <span className="text-gray-500 font-thin">{label}</span>
+    <span className="text-2xl font-bold text-blue-950">{value}</span>
+  </div>
+);
+
+// Content subcomponent
+const StatsCardContent: StatsCardType["Content"] = ({ children }) => (
+  <CardContent className="p-0">{children}</CardContent>
+);
+StatsCardContent.Trend = ({ value, timeframe }) => (
+  <p className="text-gray-500 font-thin flex gap-1">
+    {value && <span className="text-green-500 font-bold">{value}</span>}
+    <span>{timeframe}</span>
+  </p>
+);
+
+// Attach main slots
+StatsCard.Header = StatsCardHeader;
+StatsCard.Content = StatsCardContent;
+
+// All component types
+type ComponentProps = {
+  children: React.ReactNode;
+};
+// Header
+type HeaderIconProps = {
+  className?: string;
+  // TODO: Needs to be changed to that only SVGs are accepted
+  children: React.SVGProps<SVGSVGElement>;
+};
+type HeaderStatsProps = {
+  label: string;
+  value: string;
+};
+// Content
+type ContentTrendProps = {
+  value?: string;
+  timeframe: string;
+};
+
+// Main compound type
+type StatsCardType = {
+  (props: ComponentProps): JSX.Element;
+  Header: {
+    (props: ComponentProps): JSX.Element;
+    Icon: (props: HeaderIconProps) => JSX.Element;
+    Stats: (props: HeaderStatsProps) => JSX.Element;
+  };
+  Content: {
+    (props: ComponentProps): JSX.Element;
+    Trend: (props: ContentTrendProps) => JSX.Element;
+  };
 };
